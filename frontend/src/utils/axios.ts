26ddbf73e405808cfbfad0ec76875dd8bd965314
veldos <89993707+veldos@users.noreply.github.com>
2024-05-26 17:32:51 +0100

    Update axios.ts

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Replace with your backend API URL
+  baseURL: 'https://malefic.onrender.com/api', // Replace with your backend API URL
 });
 
 axiosInstance.interceptors.request.use(
