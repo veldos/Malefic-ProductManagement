8c2c353901b1f929948ba30f349983095664f3db
veldos <89993707+veldos@users.noreply.github.com>
2024-06-07 18:10:56 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import AppRoutes from './Routes';
-
+import styled from 'styled-components';
+// import { AuthProvider } from './utils/AuthContext';
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y proximity;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
 const App: React.FC = () => {
-  return <AppRoutes />;
+  return(
+        <Container>
+
+          <AppRoutes />
+        </Container>
+  
+
+  );
 };
 
 export default App;
